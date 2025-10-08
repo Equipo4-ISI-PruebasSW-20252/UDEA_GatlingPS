1bc2fbe91668ef9501e9a3a77375d7943c6acd96
Cristian Muñoz <cristian.munoz3@udea.edu.co>
2025-10-08 09:38:49 -0500

    Se cambia el id de la cuenta para que permita verificar el status dado que la anterior la borraron

--- src/test/scala/parabank/Data.scala
+++ src/test/scala/parabank/Data.scala
@@ -4,5 +4,5 @@ object Data{
     val url= "https://parabank.parasoft.com/parabank/services/bank"
     val username = "john"
     val password = "demo"
-    val accountId = "54321"
+    val accountId = "15120"
 }
